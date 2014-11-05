cf592834e2bc4e6ab7b93d8af0d85bb9331ae556
amnaha15 <amnaha15@asl.org>
2014-11-05 09:17:49 +0000

    MAJOR EDIT
    Edited how to input and work program

--- src/Runner.java
+++ src/Runner.java
@@ -8,80 +8,48 @@ public class Runner {
 
 	public static void main(String[] args) {
 
-		System.out
-				.println("Would you like to Add, Subtract, Multiply or Divide?");
+
+		//Creating necessary variables
 		Scanner user = new Scanner(System.in);
-		String command = user.nextLine().toLowerCase();
-
 		boolean loop = true;
-		String input;
-		double answer = 0.0;
-		double x;
-
-		if (command.equals("add")) {
-			while (loop == true) {
-				System.out.println("What would you like to add?");
-				input = user.next();
-				if (input.equals("bye")) {
-					loop = false; 
-					System.out.println("Your answer is " + answer);
-				} else {
-					x = Double.parseDouble(input);
+		String command;
+		double x, answer;
+		String strAnswer;
+		
+		System.out.println("Here is how the basic calculator works:");
+		System.out.println("Please enter only integer or double values,");
+		System.out.println("You will need to type a number followed enter");
+		System.out.println("Then type your operation (+ - / *) followed by enter");
+		System.out.println("To finish you will need to type '='");
+		System.out.println("Ready? Let's go!");
+		
+		answer = user.nextDouble();
+		
+		while(loop == true){
+			command = user.next();
+			x= user.nextDouble();
+				if (command.equals("+")) {
 					answer = Basic.add(answer, x);
 					System.out.println("= " + answer);
 				}
-			}
-		}
-		if (command.equals("subtract")) {
-			System.out.println("What would you like to subtract?");
-			x = user.nextDouble();
-			answer = Math.abs(Basic.subtract(answer, x));
-			System.out.println(answer);
-			while (loop == true) {
-				System.out.println("What would you like to subtract?");
-				input = user.next();
-				if (input.equals("bye")) {
-					loop = false;
-					System.out.println("Your answer is " + answer);
-				} else {
-					x = Double.parseDouble(input);
+				else if (command.equals("-")) {
 					answer = Basic.subtract(answer, x);
 					System.out.println("= " + answer);
 				}
-			}
-		}
-		if (command.equals("multiply")) {
-			answer = 1;
-			while (loop == true) {
-				System.out.println("What would you like to multiply?");
-				input = user.next();
-				if(input.equals("bye")){
-					loop = false; 
-					System.out.println("You answer is " + answer);
+				else if (command.equals("*")) {
+					answer = Basic.multiply(answer, x);
+					System.out.println("= " + answer);
 				}
-				else{
-				x = Double.parseDouble(input);
-				answer = Basic.multiply(answer, x);
-				System.out.println("= " + answer);
+				else if (command.equals("/")) {
+					answer = Basic.divide(answer, x);
+					System.out.println(answer);
 				}
-			}
-		}
-		if (command.equals("divide")) {
-			System.out.println("What would you like to divide?");
-			answer = user.nextDouble();
-			while (loop == true) {
-				System.out.println("What would you like to divide?");
-				input = user.next();
-				if(input.equals("bye")){
-					loop = false; 
-					System.out.println("You answer is " + answer);
+				else if (command.equals("=")){
+					System.out.println("Your answer is" + strAnswer);
 				}
-				else{
-				x = Double.parseDouble(input);
-				answer = Basic.divide(answer, x);
-				System.out.println(answer);
-				}
-			}
+				
+				
 		}
 	}
 }
+
