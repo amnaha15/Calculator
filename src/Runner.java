460706e676919edf5c57ca06009d62ea2efb6a7b
amnaha15 <amnaha15@asl.org>
2014-11-07 13:10:06 +0000

    Basic Complete
    Loop works perfectly for Basic
    For + - * /

--- src/Runner.java
+++ src/Runner.java
@@ -8,48 +8,61 @@ public class Runner {
 
 	public static void main(String[] args) {
 
-
-		//Creating necessary variables
+		// Creating necessary variables
 		Scanner user = new Scanner(System.in);
 		boolean loop = true;
 		String command;
 		double x, answer;
-		String strAnswer;
-		
+
 		System.out.println("Here is how the basic calculator works:");
 		System.out.println("Please enter only integer or double values,");
 		System.out.println("You will need to type a number followed enter");
-		System.out.println("Then type your operation (+ - / *) followed by enter");
+		System.out
+				.println("Then type your operation (+ - / *) followed by enter");
 		System.out.println("To finish you will need to type '='");
 		System.out.println("Ready? Let's go!");
-		
+
 		answer = user.nextDouble();
+
 		
-		while(loop == true){
+		
+		/*
+		 * Carries out basic arithmetic (including Basic and Math classes
+		 *  (+ - * / ^ %) 
+		 */
+		while (loop == true) {
 			command = user.next();
-			x= user.nextDouble();
-				if (command.equals("+")) {
-					answer = Basic.add(answer, x);
-					System.out.println("= " + answer);
-				}
-				else if (command.equals("-")) {
-					answer = Basic.subtract(answer, x);
-					System.out.println("= " + answer);
-				}
-				else if (command.equals("*")) {
-					answer = Basic.multiply(answer, x);
-					System.out.println("= " + answer);
-				}
-				else if (command.equals("/")) {
-					answer = Basic.divide(answer, x);
-					System.out.println(answer);
-				}
-				else if (command.equals("=")){
-					System.out.println("Your answer is" + strAnswer);
-				}
-				
-				
+
+			if (command.equals("=")) {
+				System.out.println("Your answer is " + answer);
+			}
+
+			x = user.nextDouble();
+
+			if (command.equals("+")) {
+				answer = Basic.add(answer, x);
+				System.out.println("= " + answer);
+			} else if (command.equals("-")) {
+				answer = Basic.subtract(answer, x);
+				System.out.println("= " + answer);
+			} else if (command.equals("*")) {
+				answer = Basic.multiply(answer, x);
+				System.out.println("= " + answer);
+			} else if (command.equals("/")) {
+				answer = Basic.divide(answer, x);
+				System.out.println(answer);
+			} else if (command.equals("^")) {
+				answer = Math.pow(answer , x);
+				System.out.println(answer);
+			}	
 		}
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
-
